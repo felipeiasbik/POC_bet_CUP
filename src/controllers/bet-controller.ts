512fbeea1b59c52ef023eed9a64bb5d0548db798
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-07-01 08:42:48 -0300

    error handling

--- src/controllers/bet-controller.ts
+++ src/controllers/bet-controller.ts
@@ -4,27 +4,47 @@ import * as betService from "../services/bet-service"
 import httpStatus from "http-status";
 
 export async function createBet(req: Request, res: Response): Promise<void>{
-    const bet = req.body as BetResult; 
-    betService.createBet(bet);
-
-    res.sendStatus(httpStatus.CREATED);
+    try {
+        const bet = req.body as BetResult; 
+        betService.createBet(bet);
+    
+        res.sendStatus(httpStatus.CREATED);
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function getBets(req: Request, res: Response): Promise<void>{
-    const list = await betService.getBets();
-    res.status(httpStatus.OK).send(list.rows);
+    try {
+        const list = await betService.getBets();
+        res.status(httpStatus.OK).send(list.rows);
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function updateBet(req: Request, res: Response): Promise<void>{
-    const bet = req.body as IdBetResult;
-    betService.updateBet(bet);
-
-    res.sendStatus(httpStatus.CREATED);
+    try {
+        const bet = req.body as IdBetResult;
+        betService.updateBet(bet);
+    
+        res.sendStatus(httpStatus.CREATED);
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function deleteBet(req: Request, res: Response): Promise<void>{
-    const id = req.body.id as idDelete;
-    betService.deleteBet(id);
-
-    res.sendStatus(httpStatus.OK);
+    try {
+        const id = req.body.id as idDelete;
+        betService.deleteBet(id);
+    
+        res.sendStatus(httpStatus.OK);
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
